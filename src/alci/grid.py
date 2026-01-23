db5d42061f4002eb5399b53f305cbcc64695dc88
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2026-01-23 21:26:46 +0800

    quantics grid

--- src/alci/grid.py
+++ src/alci/grid.py
@@ -19,3 +19,26 @@ class UniformGrid:
         grid_pts = self.all_grid_pts()
         space_pts = jax.vmap(self.grid2space)(grid_pts)
         return grid_pts, space_pts
+
+class QuanticsGrid:
+    def __init__(self, base: int, nbit: int, dim: int, start: jax.Array, end: jax.Array):
+        base
+        self.base = base
+        self.nbit = nbit
+        self.dim = dim
+        self.start = start
+        self.end = end
+    
+    def grid2space(self, grid_point: jax.Array) -> jax.Array:
+        weight = jnp.array([[n for n in range(self.nbit)] for d in range(self.dim)])
+        bits = jnp.array([[grid_point[self.dim*n+d] for n in range(self.nbit)] for d in range(self.dim)])
+        g = jnp.sum(bits * (self.base ** weight), axis=1)
+        return self.start + (self.end - self.start) * g / (self.base ** self.nbit - 1)
+    
+    def all_grid_pts(self) -> jax.Array:
+        return jnp.stack(jnp.meshgrid(*[jnp.arange(self.base) for _ in range(self.nbit * self.dim)], indexing='ij'), axis=-1).reshape(-1, self.nbit * self.dim)
+    
+    def grid_and_space(self):
+        grid_pts = self.all_grid_pts()
+        space_pts = jax.vmap(self.grid2space)(grid_pts)
+        return grid_pts, space_pts
--- test/test_grid.py
+++ test/test_grid.py
@@ -1,5 +1,5 @@
 import pytest
-from alci.grid import UniformGrid
+from alci.grid import UniformGrid, QuanticsGrid
 import jax
 import jax.numpy as jnp
 
@@ -26,3 +26,18 @@ def test_grid_and_space():
     gp, sp = g.grid_and_space()
     assert gp.shape == (100, 2)
     assert sp.shape == (100, 2)
+
+def test_quantics_construct():
+    g = QuanticsGrid(2, 4, 2, jnp.array([0.0, 0.0]), jnp.array([1.0, 1.0]))
+    assert g.base == 2
+    assert g.nbit == 4
+    assert g.dim == 2
+    assert jnp.allclose(g.start, jnp.array([0.0, 0.0]))
+    assert jnp.allclose(g.end, jnp.array([1.0, 1.0]))
+
+def test_quantics_grid2space():
+    g = QuanticsGrid(2, 4, 2, jnp.array([0.0, 0.0]), jnp.array([1.0, 1.0]))
+    assert jnp.allclose(g.grid2space(jnp.array([0, 0, 0, 0, 0, 0, 0, 0])), g.start)
+    assert jnp.allclose(g.grid2space(jnp.array([1, 1, 1, 1, 1, 1, 1, 1])), g.end)
+    assert jnp.allclose(g.grid2space(jnp.array([0, 1, 0, 1, 0, 1, 0, 1])), jnp.array([0.0, 1.0]))
+    assert jnp.allclose(g.grid2space(jnp.array([1, 0, 1, 0, 1, 0, 1, 0])), jnp.array([1.0, 0.0]))
